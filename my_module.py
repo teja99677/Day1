23d109c3b75bbb48404e80f4527c3adcd745444b
teja99677 <tejam5725@gmail.com>
2025-01-24 22:17:39 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+greet = lambda name : print(f"Good Morning {name}!")
+greet("Sumith")
+
+
+# In[8]:
+
+
+#product of three numbers 
+product = lambda num1,num2,num3:print(num1*num2*num3)
+
+product(3,3,2)
+
+
+# In[9]:
+
+
+#lambda function with list comprehension
+even = lambda L : [x for x in L if x%2 == 0]
+my_list = [100,3,943,77]
+even(my_list)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
